4116f8ddc616ce52458bb3b083a8fb2a59d29c55
Al Duncanson <alexjduncanson@gmail.com>
2022-09-07 00:13:54 -0400

    config and packages

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,20 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect } from 'react'
+import logo from './logo.svg'
+import './App.css'
 
 function App() {
+
+	useEffect(() => {
+		console.log('☕️')
+	}, [])
+
 	return (
 		<div className="App">
 			<header className="App-header">
 				<img src={logo} className="App-logo" alt="logo" />
 			</header>
 		</div>
-	);
+	)
 }
 
-export default App;
+export default App
