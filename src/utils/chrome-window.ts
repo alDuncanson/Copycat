7618169845af03de08b1fa1e7cb5e5f5506f430e
Al Duncanson <alexjduncanson@gmail.com>
2022-09-12 21:49:07 -0400

    add `chrome.windows` utilities

---
+++ src/utils/chrome-window.ts
@@ -0,0 +1,14 @@
+export function togglePopupWindow() {
+	chrome.windows.getCurrent(window => {
+		if (window.type === 'popup' && window.id) {
+			chrome.windows.remove(window.id)
+		} else {
+			chrome.windows.create({
+				url: '/index.html',
+				type: 'popup',
+				width: 400,
+				height: 600,
+			})
+		}
+	})
+}
